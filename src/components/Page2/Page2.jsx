c277815a04c7d48f624d18fcac32e17c88570639
DhirenAtodaria <dhiren.atodaria@hotmail.co.uk>
2020-04-28 19:07:58 +0100

    error before moving left

---
+++ src/components/Page2/Page2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Slideshow from "../Slideshow/Slideshow";
+import "./Page2.scss";
+
+export default function Page2() {
+    return (
+        <div className="container2">
+            <h1>elysium planitia weather</h1>
+            <div className="root">
+                <Slideshow />
+            </div>
+        </div>
+    );
+}
--- src/features/dataFetcher/dataFetcherSlice.js
+++ src/features/dataFetcher/dataFetcherSlice.js
@@ -5,20 +5,34 @@ export const fetchJsonData = createAsyncThunk(
     "data/fetchJsonData",
     async (thunkAPI) => {
         const response = await axios.get(
-            "https://jsonplaceholder.typicode.com/posts"
+            "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
         );
-        return response.data;
+        const data = response.data;
+        delete data.sol_keys;
+        delete data.validity_checks;
+        let items = [];
+        for (let key in data) {
+            data[key]["SOL"] = key;
+            const date = new Date(data[key].First_UTC);
+            const newDate = `${date.getDate()} ${date.toLocaleString(
+                "default",
+                { month: "long" }
+            )}`;
+            data[key]["First_UTC"] = newDate;
+            items.push(data[key]);
+        }
+        return items;
     }
 );
 
 export const dataFetcherSlice = createSlice({
     name: "dataFetcher",
     initialState: {
-        posts: [],
+        apiData: [],
     },
     extraReducers: {
         [fetchJsonData.fulfilled]: (state, action) => {
-            state.posts = action.payload;
+            state.apiData = action.payload;
         },
     },
 });
