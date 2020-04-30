bc0772f550ed75138c4a37e4f04e3dfd41f5677b
DhirenAtodaria <dhiren.atodaria@hotmail.co.uk>
2020-04-30 16:24:45 +0100

    Updates

---
+++ precache-manifest.41863f9e5d0e00b595eac8cf8de62244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b373eba235fc5fdb8ccc294586ed85a",
+    "url": "/nasa-project/index.html"
+  },
+  {
+    "revision": "7dc2e50e631001cda8f2",
+    "url": "/nasa-project/static/css/main.f91402c4.chunk.css"
+  },
+  {
+    "revision": "3d7ed9ef54875f3c3108",
+    "url": "/nasa-project/static/js/2.8381f4fa.chunk.js"
+  },
+  {
+    "revision": "51ccd56507b155fc340d16b580bc1a5d",
+    "url": "/nasa-project/static/js/2.8381f4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc2e50e631001cda8f2",
+    "url": "/nasa-project/static/js/main.3d3fc015.chunk.js"
+  },
+  {
+    "revision": "174b89f5e1b41ef893d9",
+    "url": "/nasa-project/static/js/runtime-main.200f058f.js"
+  },
+  {
+    "revision": "7775b2ab8bdaa25788912b375b09d9f4",
+    "url": "/nasa-project/static/media/Questrial-Regular.7775b2ab.ttf"
+  },
+  {
+    "revision": "32481d990578cb7aefa194c7f3992533",
+    "url": "/nasa-project/static/media/bh2.32481d99.jpg"
+  },
+  {
+    "revision": "e7f45e24690e1e6903e9036ebd801d3c",
+    "url": "/nasa-project/static/media/black_marble___americas-wallpaper-3840x2160.e7f45e24.jpg"
+  },
+  {
+    "revision": "9decaf0b165976ed6a2d4a51655a0d86",
+    "url": "/nasa-project/static/media/nasa-2-logo-png-transparent.9decaf0b.png"
+  },
+  {
+    "revision": "17d7207cef982f2e7333db1cedca756f",
+    "url": "/nasa-project/static/media/trial4.17d7207c.jpg"
+  }
+]);
